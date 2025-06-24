2fdc70bc40cc09377ce5c74c6cba05c3eac46d91
anpan <agni.infinity@gmail.com>
2025-06-24 19:01:45 +0700

    chore: Update formatting

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, NotFoundException, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 
 class Tag {
   id: number;
